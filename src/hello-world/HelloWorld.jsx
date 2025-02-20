8b83247ce7c6bb382ae572c9f8f084aa65a96492
khairulharu <khairulaswad719@gmail.com>
2025-02-20 11:46:40 +0800

    init react

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+     return (
+          <div>
+               <h1>belajar react dasar</h1>
+               <p>belajar react dasar</p>
+          </div>
+     )
+}
