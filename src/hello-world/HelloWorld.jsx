b311feb97e5b689ffb56001d0ac1493fe7ee5ffd
khairulharu <khairulaswad719@gmail.com>
2025-02-21 17:57:01 +0800

    component

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,8 +1,29 @@
-export default function HelloWorld() {
+export function HelloWorld() {
      return (
           <div>
-               <h1>belajar react dasar</h1>
-               <p>belajar react dasar</p>
+               <HeaderHelloWorld />
+               <ParagraphHelloWorld />
+          </div>
+     )
+}
+
+export default HelloWorld;
+
+export function ParagraphHelloWorld() {
+     const text = "belajar react dasar halo dek, terjadi perubahan, test hot reloading"
+     return (
+          <div>
+               <p>{ text.toUpperCase() }</p>
+          </div>
+          )
+}
+
+export function HeaderHelloWorld() {
+     const text = "belajar react dasar hello kamu"
+     return (
+          <div>
+               <h1 style={{ color:"red", backgroundColor:"aqua" }}>{text}</h1>
+
           </div>
      )
 }
