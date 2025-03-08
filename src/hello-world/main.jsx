eefe391b171dce27bdb4c4acf9acd328ced9c354
khairulharu <khairulaswad719@gmail.com>
2025-03-09 04:51:30 +0800

    Learn react

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import HelloWorld from "./HelloWorld";
 import Container from "./Container";
 import './main.css'
+import TodoList from "../todolist/TodoList";
+import Table from "../table/Table";
 
 createRoot(document.getElementById("root")).render(
      <StrictMode>
         <Container>
             <HelloWorld />
+            <TodoList />
+            <Table/>
         </Container>
-     </StrictMode>
+     </StrictMode> 
 )
