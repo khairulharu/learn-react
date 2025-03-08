6149a57793b9b5ac03651a0d0dc0b3de45a03107
khairulharu <khairulaswad719@gmail.com>
2025-03-09 04:44:00 +0800

    React nested component

---
+++ src/todolist/TodoList.jsx
@@ -0,0 +1,38 @@
+import Todo from "./Todo";
+
+export default function TodoList() {
+     const data = [
+          {
+               isCompleted: true,
+               text: "mabar epep",
+          },
+          {
+               isCompleted: true,
+               text: "mabar pabji",
+          },
+          {
+               isCompleted: true,
+               text: "mabar vscode"
+          },
+          {
+               isCompleted: true,
+               text: "mabar mswors"
+          },
+          {
+               isCompleted: false,
+               text: "mabar youtube"
+          },
+          {
+               isCompleted: false,
+               text: "mabar linkedin                             "
+          }
+     ]
+
+     return (
+          <ul>
+               {data.map((todo, index) => (
+                    <Todo key={index} {...todo}/>
+               ))}
+          </ul>
+     )
+}
