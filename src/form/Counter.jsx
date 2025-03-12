3cf025ecd8714964d63ee22542f646df43df1538
khairulharu <khairulaswad719@gmail.com>
2025-03-12 22:11:27 +0800

    learn state snapshot data

--- src/form/Counter.jsx
+++ src/form/Counter.jsx
@@ -3,16 +3,18 @@ import { useState } from "react";
 export default function Counter() {
      let [counter, setCounter] = useState(0)
 
-     console.log("console log: rendering again")
+     console.log(`current number: ${counter}`)
 
      function handleClick() {
-          setCounter(counter + 1)
+          setCounter((c) => c + 1)
+          setCounter((c) => c + 1) 
+          setCounter((c) => c + 1)     
           console.log(counter)
      }
      return (
           <div>
                <div>
-                    <button onClick={handleClick}>Increment</button>
+                    <button onClick={handleClick}>Increment + 3</button>
                </div>
                <h1>Counter : {counter}</h1>
           </div>
