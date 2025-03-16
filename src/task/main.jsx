fc6eebdbea7e4d119c70268e63d440319da87de5
khairulharu <khairulaswad719@gmail.com>
2025-03-16 20:49:25 +0800

    Refactor: add new component task

---
+++ src/task/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+     <StrictMode>
+          <p>root task main jsx applied succesffuly</p>
+     </StrictMode>
+)
