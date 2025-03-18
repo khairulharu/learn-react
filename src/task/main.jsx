465e70a876b34cb07114433e7e0aa1ae358b4877
khairulharu <khairulaswad719@gmail.com>
2025-03-18 16:27:31 +0800

    refactor anothe

--- src/task/main.jsx
+++ src/task/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Task from "./Task";
 
 createRoot(document.getElementById("root")).render(
      <StrictMode>
-          <p>root task main jsx applied succesffuly</p>
+          <Task/>
      </StrictMode>
 )
