5db202d8f92c3ba25f9885ad0c500e2b11622ab8
khairulharu <khairulaswad719@gmail.com>
2025-03-20 21:44:32 +0800

    Refactor state sharing

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,52 +1,20 @@
-import { useState } from "react"
 import { useImmer } from "use-immer";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 export default function Task() {
-     const [item, setItem] = useState("");
-     const [items, setItems] = useImmer([]);
+     const [items, setItems] = useImmer([])
 
-     function handleChange(e) {
-          setItem(e.target.value);
-     }
-
-     function handleClick(e) {
-          e.preventDefault()
-          
-          if (item === "") {
-               alert("you must be type something");
-          } else {
-               setItems((draft) => {
-                    draft.push(item);
-               })
-               setItem(""); 
-          }
-     }
-
-     function handleButtonClick(e) {
-
-          console.log(e.target.value)
+     function handleSubmit(item) {
           setItems((draft) => {
-               draft.filter((value)=> {
-                    value != e.target.value
-               })
+               draft.push(item);
           })
      }
 
      return (
           <div>
-               <h1>Create Task</h1>
-               <form>
-                    <input type="text" placeholder="add task here" value={item} onChange={handleChange}/>
-                    <button onClick={handleClick}>Add</button>
-               </form>
-               <h1>List Task</h1>
-               <ul>
-                    {items.map((item, index) =>
-                         <div key={index}>
-                              <li>{item}</li> <button id="index" onClick={handleButtonClick} value={item}>DELETE</button>
-                         </div>
-                    )}
-               </ul>
+               <TaskForm onSubmit={handleSubmit}/>
+               <TaskList items={items}/>
           </div>
      )
 }
