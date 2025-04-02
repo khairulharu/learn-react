75cb28d560c85ac51237bae1ef52f4545453a777
khairulharu <khairulaswad719@gmail.com>
2025-04-02 15:45:26 +0800

    learn how to use reducer

--- src/note/NoteApp.jsx
+++ src/note/NoteApp.jsx
@@ -1,6 +1,6 @@
-import { useImmer } from "use-immer";
 import NoteForm from "./NoteForm";
 import NoteList from "./NoteList";
+import { useReducer } from "react";
 
 let id = 0;
 const initialNotes = [
@@ -10,31 +10,52 @@ const initialNotes = [
      {id: id++, text: "learn REACT", done: false},
 ];
 
+function notesReducer(notes, action) {
+     switch (action.type) {
+          case "ADD_NOTE":
+               return [
+                    ...notes,
+                    {
+                         id: id++,
+                         text: action.text,
+                         done: false,
+                    }
+               ];
+          case "CHANGE_NOTE":
+               return notes.map(note => 
+                    note.id === action.id ? {...note, text: action.text, done: action.done} : note
+               );
+          case "DELETE_NOTE":
+               return notes.filter(note => note.id !== action.id);
+          default:
+               return notes;
+     }
+}
+
 export default function NoteApp() {
-     const [notes, setNotes] = useImmer(initialNotes);
+     const [notes, dispatch] = useReducer(notesReducer, initialNotes);
 
      function handleAddNote(text) {
-          setNotes(draft => {
-               draft.push({
-                    id: id++,
-                    text: text,
-                    done: false
-               });
-          });
+          dispatch({
+               type: "ADD_NOTE",
+               text: text
+          })
      }
 
      function handleChangeNote(note) {
-          setNotes(draft => {
-               const index = draft.findIndex(item => item.id === note.id);
-               draft[index] = note
-          });
+          dispatch({
+               type:"CHANGE_NOTE",
+               id: note.id,
+               text: note.text,
+               done: note.done
+          })
      }
 
      function handleDeleteNote(note) {
-          setNotes(draft => {
-               const index = draft.findIndex(item => item.id === note.id);
-               draft.splice(index, 1)
-          });
+          dispatch({
+               type: "DELETE_NOTE",
+               id: note.id
+          })
      }
      return (
           <div>
