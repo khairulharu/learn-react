f94575597a9edd27a8e195270468ac43da9b0c29
khairulharu <khairulaswad719@gmail.com>
2025-04-06 10:48:13 +0800

    How to use context and state

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Profile from "./Profile";
 import ProfileAddress from "./ProfileAddress";
 import { ProfileContext } from "./ProfileContext";
+import ProfileForm from "./ProfileForm";
 
 export default function ProfileApp() {
+     const [name, setName] = useState("Set Your Name");
+
      return (
           <>
-          <ProfileContext.Provider value="halo dek inii adalah context">
+          <ProfileContext.Provider value={name}>
+               <ProfileForm name={name} setName={setName}/>
                <h1>Profile App</h1>
                <Profile />
                <ProfileAddress />
